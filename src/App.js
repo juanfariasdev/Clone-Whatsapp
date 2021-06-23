d8c5acc815e766358b3cb3174f37601fa6e0e649
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2021-06-22 22:10:39 -0300

    Iniciando o projeto no git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import ChatListitem from './components/ChatListItem';
+import ChatIntro from './components/ChatIntro';
+import ChatWindow from './components/ChatWindow';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default () => {
+
+  const[chatlist, setChatlist] = useState([
+    {chatId: 1, title: 'Fulano', image: 'https://image.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg'},
+    {chatId: 2, title: 'Fulano', image: 'https://image.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg'},
+    {chatId: 3, title: 'Fulano', image: 'https://image.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg'},
+    {chatId: 4, title: 'Fulano', image: 'https://image.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg'},
+  ]);
+  const [activeChat, setActiveChat] = useState({});
+  return(
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img className="header--avatar" src="https://image.freepik.com/vetores-gratis/avatar-de-personagem-de-empresario-isolado_24877-60111.jpg" alt=""/>
+          <div className="header--buttons">
+            <div className="header--btn">
+              <DonutLargeIcon style={{color: '#919191'}}/>
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{color: '#919191'}}/>
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{color: '#919191'}}/>
+            </div>
+          </div>
+        </header>
+        <div className="search">
+          <div className="search--input">
+            <SearchIcon fontSize="small" style={{color: '#919191'}} />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa" />
+          </div>
+        </div>
+        <div className="chatList">
+          {chatlist.map((item, key) => (
+            <ChatListitem 
+              key={key}
+              data={item}
+              active={activeChat.chatId === chatlist[key].chatId}
+              onClick={()=> setActiveChat(chatlist[key])}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="contentarea">
+        {activeChat.chatId !== undefined && 
+          <ChatWindow />
+        }
+        {activeChat.chatId === undefined &&
+        <ChatIntro />
+        }
+      </div>
     </div>
   );
-}
 
-export default App;
+}
---
+++ src/components/ChatIntro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ChatIntro.css';
+
+export default () => {
+    return (
+        <div className="chatIntro">
+            <img src="https://images.vexels.com/media/users/3/136865/isolated/preview/7221ccf73801a7e9729d11d505fcff2b-smartphone-dispon-iacute-vel-by-vexels.png" alt="" />
+            <h1>Seja muito Bem Vindo!</h1>
+            <h2>Esse software está sendo criado a fins de estudos. Fico Feliz de ter você vendo meu portfólio!</h2>
+        </div>
+    );
+}
