ddf1ca46512010ef76a07f28e975b39cf3e330f0
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2021-06-27 22:52:27 -0300

    ajuste

--- src/Api.js
+++ src/Api.js
@@ -72,6 +72,10 @@ export default{
                         if(a.lastMessageDate === undefined){
                             return -1;
                         }
+                        if(b.lastMessageDate === undefined){
+                            return -1;
+                        }
+                        
                         if(a.lastMessageDate.seconds < b.lastMessageDate.seconds){
                             return 1;
                         } else {
